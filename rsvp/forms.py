08f555daa482cb7654779187701a25dcc905397f
polarcowz <polarcowz@594bd101-6c55-0410-b075-b54a68c56e47>
2008-09-19 02:07:59 +0000

    Refactored views/forms for better reusability.
    git-svn-id: https://django-rsvp.googlecode.com/svn/trunk@6 594bd101-6c55-0410-b075-b54a68c56e47

--- rsvp/forms.py
+++ rsvp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ObjectDoesNotExist
 from rsvp.models import ATTENDING_CHOICES, Guest
 
 
@@ -12,13 +13,21 @@ class RSVPForm(forms.Form):
     number_of_guests = forms.IntegerField(initial=0)
     comment = forms.CharField(max_length=255, required=False, widget=forms.Textarea)
     
+    def __init__(self, *args, **kwargs):
+        if 'guest_class' in kwargs:
+            self.guest_class = kwargs['guest_class']
+            del(kwargs['guest_class'])
+        else:
+            self.guest_class = Guest
+        super(RSVPForm, self).__init__(*args, **kwargs)
+    
     def clean_email(self):
         try:
-            guest = Guest.objects.get(email=self.cleaned_data['email'])
-        except Guest.DoesNotExist:
+            guest = self.guest_class._default_manager.get(email=self.cleaned_data['email'])
+        except ObjectDoesNotExist:
             raise forms.ValidationError, 'That e-mail is not on the guest list.'
         
-        if guest.attending_status != 'no_rsvp':
+        if hasattr(guest, 'attending_status') and guest.attending_status != 'no_rsvp':
             raise forms.ValidationError, 'You have already provided RSVP information.'
         
         return self.cleaned_data['email']
@@ -29,7 +38,7 @@ class RSVPForm(forms.Form):
         return self.cleaned_data['number_of_guests']
         
     def save(self):
-        guest = Guest.objects.get(email=self.cleaned_data['email'])
+        guest = self.guest_class._default_manager.get(email=self.cleaned_data['email'])
         
         if self.cleaned_data['name']:
             guest.name = self.cleaned_data['name']
--- rsvp/views.py
+++ rsvp/views.py
@@ -1,39 +1,40 @@
 import datetime
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from rsvp.models import Event, Guest
 from rsvp.forms import RSVPForm
 
 
-def event_view(request, slug):
-    event = get_object_or_404(Event, slug=slug)
+def event_view(request, slug, model_class=Event, form_class=RSVPForm, template_name='rsvp/event_view.html'):
+    event = get_object_or_404(model_class, slug=slug)
     
     if request.POST:
-        form = RSVPForm(request.POST)
+        form = form_class(request.POST)
         
         if form.is_valid():
             guest = form.save()
             return HttpResponseRedirect(reverse('rsvp_event_thanks', kwargs={'slug': slug, 'guest_id': guest.id}))
     else:
-        form = RSVPForm()
+        form = form_class()
     
-    return render_to_response('rsvp/event_view.html', {
+    return render_to_response(template_name, {
         'event': event,
         'form': form,
     }, context_instance=RequestContext(request))
 
 
-def event_thanks(request, slug, guest_id):
-    event = get_object_or_404(Event, slug=slug)
+def event_thanks(request, slug, guest_id, model_class=Event, template_name='rsvp/event_thanks.html'):
+    event = get_object_or_404(model_class, slug=slug)
     
     try:
         guest = event.guests.get(pk=guest_id)
-    except Guest.DoesNotExist:
+    except ObjectDoesNotExist:
         raise Http404
     
-    return render_to_response('rsvp/event_thanks.html', {
+    return render_to_response(template_name, {
         'event': event,
         'guest': guest,
     }, context_instance=RequestContext(request))
